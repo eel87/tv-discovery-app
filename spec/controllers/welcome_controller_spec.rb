5e34d6d4ff07f57333837ab670b8c40da77ccb02
erinleck@gmail.com <erinleck@gmail.com>
2019-12-10 22:33:13 -0700

    add welcome controller test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe WelcomeController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the index view" do
+      get :index, params: @popular_shows
+      expect(response).to render_template(:index)
+    end
   end
 end
